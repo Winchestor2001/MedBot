ad7d92a83cf991b3978ee6c4cdc35810012b5d2d
Muzaffar-Xadjayev <muzaffar010125@gmail.com>
2023-10-25 00:31:19 +0500

    ...

--- med_app/serializers.py
+++ med_app/serializers.py
@@ -8,10 +8,32 @@ class UserSerializer(ModelSerializer):
         fields = ("user_id", "username", "is_doctor")
 
 
+class ServiceSerializer(ModelSerializer):
+
+    class Meta:
+        model = Doctor
+        fields = '__all__'
+
+
 class DoctorSerializer(ModelSerializer):
     class Meta:
         model = Doctor
-        fields = ["user", "full_name", "direction", "price", "experience", "services", "reviews", "busy"]
+        fields = '__all__'
+
+    # def get_children(self, obj):
+    #     # print(obj.date_set.all())
+    #     i = ServiceSerializer(data=obj.date_set.all(), many=True)
+    #     print(i)
+    #     i.is_valid()
+    #
+    #     return i.data
+    #
+    # def to_representation(self, instance):
+    #     ret = super().to_representation(instance)
+    #
+    #     if self.get_children(instance):
+    #         ret['children'] = self.get_children(instance)
+    #     return ret
 
 
 class PatientSerializer(ModelSerializer):
