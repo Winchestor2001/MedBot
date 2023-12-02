4ff23a55cf17983a4f4f159b0096e7d65909771f
winchestor2001 <sbexruz2001@gmail.com>
2023-12-02 13:04:34 +0500

    some changes

--- med_app/tasks.py
+++ med_app/tasks.py
@@ -2,8 +2,10 @@ from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from core.celery import app
 from .models import MeetingRoom
+from datetime import datetime
 
 
 @app.task
 def check_meet():
-    pass
+    now_time = datetime.now()
+    meets = MeetingRoom.objects.all()
