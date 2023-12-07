265130793b21c5ac5d20df036da067e798fe8679
winchestor2001 <sbexruz2001@gmail.com>
2023-12-07 22:25:15 +0500

    fix swagger

--- med_app/views.py
+++ med_app/views.py
@@ -180,7 +180,7 @@ class PatientResultApiView(APIView):
 
     @swagger_auto_schema(
         operation_summary="Create patient result (web)",
-        request_body=patient_result_post_param
+        manual_parameters=[patient_result_post_param]
     )
     def post(self, request):
         patient = Patient.objects.get(id=request.data["patient"])
