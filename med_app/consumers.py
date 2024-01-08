7c33f6578c83087c59dd0816f679fda5f79b9d51
Winchestor2001 <sbexruz2001@gmail.com>
2024-01-08 10:53:14 +0500

    start branch coding

--- med_app/consumers.py
+++ med_app/consumers.py
@@ -90,32 +90,32 @@ class VideoConsumer2(AsyncWebsocketConsumer):
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.users_count = 0
+
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
 
         # Join room group
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-
+        self.users_count += 1
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Leave room group
+        self.users_count -= 1
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
-    # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
-        # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name, {"type": "chat.message", "message": message}
         )
 
-    # Receive message from room group
     async def chat_message(self, event):
         message = event["message"]
 
-        # Send message to WebSocket
         await self.send(text_data=json.dumps({"message": message}))
