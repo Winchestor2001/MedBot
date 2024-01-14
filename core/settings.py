b9898f58efad734a695a5fd4f2c38cf3d2734bc6
Winchestor2001 <sbexruz2001@gmail.com>
2024-01-14 20:31:51 +0500

    update code and fix bug

--- core/settings.py
+++ core/settings.py
@@ -82,24 +82,24 @@ ASGI_APPLICATION = 'core.asgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 # sqlite3
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-   }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
 
 # postgresql
-# DATABASES = {
-#        'default': {
-#            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#            'NAME': env.str("DB_NAME"),
-#            'USER': env.str("DB_USER"),
-#            'PASSWORD': env.str("DB_PASSWORD"),
-#            'HOST': env.str("DB_HOST"),
-#            'PORT': env.int("DB_PORT"),
-#        }
-# }
+DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': env.str("DB_NAME"),
+           'USER': env.str("DB_USER"),
+           'PASSWORD': env.str("DB_PASSWORD"),
+           'HOST': env.str("DB_HOST"),
+           'PORT': env.int("DB_PORT"),
+       }
+}
 
 CHANNEL_LAYERS = {
     'default': {
