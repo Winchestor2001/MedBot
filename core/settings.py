20318e6f372b49cf2d1792665feb1cbc30515511
Winchestor2001 <sbexruz2001@gmail.com>
2024-03-14 01:03:19 +0500

    update login to stat page

--- core/settings.py
+++ core/settings.py
@@ -272,3 +272,4 @@ PAYMENT_SECRET_KEY = env.str('PAYMENT_SECRET_KEY')
 PAYMENT_API_KEY = env.str('PAYMENT_API_KEY')
 PAYMENT_DOMAIN = env.str('PAYMENT_DOMAIN')
 
+LOGIN_URL = 'admin/login/?next=/stat/'
